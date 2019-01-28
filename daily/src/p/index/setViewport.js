ad29b0fe82bb22f038bd3d590d16290ba8a7f322
wb-syf513486 <alibaba.inc.com>
2019-01-28 16:43:23 +0800

    ok

--- daily/src/p/index/setViewport.js
+++
@@ -1,6 +0,0 @@
-const scale = window.devicePixelRatio ? 1 / devicePixelRatio : 1;
-const metaEl = document.createElement('meta');
-metaEl.setAttribute('name', 'viewport');
-metaEl.setAttribute('content', `target-densitydpi=device-dpi, user-scalable=no, initial-scale=${scale}, maximum-scale=${scale}, minimum-scale=${scale}`);
-const headEl = document.querySelector('head');
-headEl.appendChild(metaEl);
